ec9f00c891bd4f0b7d7625b204c2a6a471f2a060
saranbsoman1 <sarangreat1997@gmail.com>
2019-09-18 23:15:18 +0530

    small and large

---
+++ largest.py
@@ -0,0 +1,7 @@
+print ("Largest Nummber")
+a=int(input("enter first no : "))
+b=int(input("enter second no : "))
+if(a>b):
+    print(a)
+else:
+    print(b)    
---
+++ smallest.py
@@ -0,0 +1,9 @@
+print("smallest no")
+a=int(input("enter first no : "))
+b=int(input("enter second no : "))
+if(a>b):
+    print(b) 
+    print("is smallest")
+else:
+    print(a) 
+    print("is smallest")    
