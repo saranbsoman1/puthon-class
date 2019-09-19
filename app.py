74c64847e58fb58c883d9ff8c88259e58b8cff2f
saranbsoman1 <sarangreat1997@gmail.com>
2019-09-19 21:55:28 +0530

    website

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "welcome"
+    return render_template('index.html')
 
 @app.route("/home") 
 def home():
     return "welcome to my homepage"   
+
+@app.route("/contact") 
+def contact():
+    return "welcome to my contact"    
 
 
 if(__name__=="__main__"):
