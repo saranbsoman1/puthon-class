15479166fff8e2a8ae8db5cb373d91956fae64c2
saranbsoman1 <sarangreat1997@gmail.com>
2019-09-19 18:59:00 +0530

    module and function

--- add.py
+++ add.py
@@ -2,3 +2,7 @@ a=int(input("enter first no : "))
 b=int(input("enter second no : "))
 s=a+b
 print(s)
+import calculator
+a=int(input("enter first no : "))
+b=int(input("enter second no : "))
+calculator.add(a,b)
